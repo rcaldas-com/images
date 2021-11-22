d6f806b90bae89f3c9174f0251ac7b141959a0cb
Robson Caldas <p25@rcaldas.com>
2021-11-21 21:53:51 -0300

    Redimenciona imagens e reduz a qualidade

--- images.py
+++ images.py
@@ -1,3 +1,9 @@
 #!/bin/env python3
+import os
+from PIL import Image
 
-print('Hello!')
+for fname in os.listdir('files'):
+    if fname.endswith(".jpg"):
+        image = Image.open(os.path.join('files', fname))
+        image.thumbnail(size=(1620,1620))
+        image.save(os.path.join('files', fname), optimize=True, quality=60)
